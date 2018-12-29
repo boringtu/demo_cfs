f32527cc7f64f0ab8346a6dcb6e9a692097c601a
李安然 <liar@alphaidea.cn>
2018-12-29 15:20:23 +0800

    * emmm

--- src/components/dialogue/visitorInfo/index.vue
+++ src/components/dialogue/visitorInfo/index.vue
@@ -5,5 +5,30 @@
 		h2 客户信息
 	.content-box
 		form
+			el-row
+				el-col(:span="12")
+					label 姓名
+					el-input
+				el-col(:span="12")
+					label 地区
+					el-input
+			el-row
+				el-col(:span="12")
+					label 性别
+					el-input
+				el-col(:span="12")
+					label 手机
+					el-input
+			el-row
+				el-col(:span="12")
+					label 对话主题
+					el-input
+				el-col(:span="12")
+					label 对话级别
+					el-input
+			el-row
+				el-col(:span="24")
+					label 备注
+					el-input(type="textarea")
 </template>
 <script lang="coffee" src="./index.coffee"></script>
