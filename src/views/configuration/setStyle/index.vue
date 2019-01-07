849751938e864dbaae0c145193668f6b459cb2f3
zhangsy <zhangsy@alphaidea.cn>
2019-01-07 18:37:54 +0800

    + 设置样式页面

--- src/views/configuration/setStyle/index.vue
+++ src/views/configuration/setStyle/index.vue
@@ -1,5 +1,21 @@
 <style lang="sass" src="./index.sass" scoped></style>
 <template lang="pug">
 .root
+    .cont_wrap
+        h1 桌面对话窗口
+        .cont_box
+            .left_set_logo
+                h2 设置logo图片
+                .common_line
+                    .left_text logo图片
+                    .right_cont 
+                        .upload_img_box
+                            .balckBg 点击上传
+                            img(src="")
+                        p 企业标志图片尺寸为180*50，图片格式为jpg，png    
+
+            .right_view_box
+                h2 预览
+
 </template>
 <script lang="coffee" src="./index.coffee"></script>
